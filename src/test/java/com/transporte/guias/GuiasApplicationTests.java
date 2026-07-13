774fb962437d560677c46d0c932120126045d4a1
Miguel G <miguel.gar.ven@gmail.com>
2026-07-13 19:53:32 -0400

    test

--- src/test/java/com/transporte/guias/GuiasApplicationTests.java
+++ src/test/java/com/transporte/guias/GuiasApplicationTests.java
@@ -3,6 +3,8 @@ package com.transporte.guias;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+//test
+
 @SpringBootTest
 class GuiasApplicationTests {
 
