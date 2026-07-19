e29707c1b13d9a2f0621fc13e062d38444853eb2
Miguel G <miguel.gar.ven@gmail.com>
2026-07-19 03:11:54 -0400

    Remove test comment from GuiasApplication.java

--- src/main/java/com/transporte/guias/GuiasApplication.java
+++ src/main/java/com/transporte/guias/GuiasApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 import com.transporte.guias.config.S3BucketProperties;
-//test
+
 @SpringBootApplication
 @EnableConfigurationProperties(S3BucketProperties.class)
 public class GuiasApplication {
